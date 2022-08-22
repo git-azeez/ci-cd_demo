7b086e1bbb1511662263db9382fbaeefef9a9103
git-azeez <87197563+git-azeez@users.noreply.github.com>
2022-08-22 16:42:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def hello_world(request):
     elif request_json and 'message' in request_json:
         return request_json['message']
     else:
-        return f'Hello Azeez this for slack integration Azeez3!!!'
+        return f'Hello Azeez this for slack integration of only  build success !!!'
